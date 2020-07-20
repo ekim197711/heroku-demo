05fd85541c2cdb8db8fb8681a050dffdfe71a70c
Mike Møller Nielsen <mmni@protonmail.com>
2020-07-20 22:49:12 +0200

    changed port

--- src/test/java/com/example/mikesherokudemo/MikesHerokuDemoApplicationTests.java
+++ src/test/java/com/example/mikesherokudemo/MikesHerokuDemoApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.mikesherokudemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class MikesHerokuDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        fail();
     }
 
 }
